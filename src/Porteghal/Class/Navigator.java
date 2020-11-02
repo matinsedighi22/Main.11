580b9ebf8df232e0834053bf3cff831d5ad939df
matinsedighi22 <73821668+matinsedighi22@users.noreply.github.com>
2020-11-03 02:26:20 +0330

    Initial commit

---
+++ src/Porteghal/Class/Navigator.java
@@ -0,0 +1,16 @@
+package Porteghal.Class;
+
+/**
+ *  this class navigate us to the target classes
+ */
+public class Navigator {
+
+
+    /**
+     * Let's go
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Navigator");
+    }
+}
